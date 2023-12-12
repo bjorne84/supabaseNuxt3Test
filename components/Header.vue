0db651e5d856f8d8d04ee6046aa5b7e7ff7dc1d4
bjorne84 <edinbjorn@gmail.com>
2023-12-12 20:32:17 +0100

    first and last commit

---
+++ components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <nav>
+            <ul>
+                <li>
+                    <nuxt-link to="/">home</nuxt-link>
+                </li>
+                <li>
+                    <nuxt-link to="/test">test</nuxt-link>
+                </li>
+                <li>
+                    <nuxt-link to="/login">login</nuxt-link>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped></style>
---
+++ layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="flex flex-col min-h-screen">
+        <Header />
+        <div class="flex-grow w-full flex items-center relative">
+            <slot />
+        </div>
+    </div>
+</template>
